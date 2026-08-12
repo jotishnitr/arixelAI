f5bf8b96d3da13346b290b4343b423bcf466084f
Jotish <jotish@gmail.com>
2026-08-12 15:39:45 +0530

    fixed bugs in server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://jotishnitr.github.io"],
     credentials: true,
   }),
 );
