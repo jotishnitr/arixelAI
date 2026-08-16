c1f39a266b68ec7a28e37853243864c798119998
Jotish <jotish@gmail.com>
2026-08-16 14:20:36 +0530

    fix: add arixelAI base path to frontendURL

--- server/controllers/requestResetPassword.js
+++ server/controllers/requestResetPassword.js
@@ -16,7 +16,7 @@ const requestPasswordReset = async (req, res, next) => {
         const token = jwt.sign({ id: user.userId, email: user.email }, secret, { expiresIn: '1h' });
 
         // URL should point to your frontend application's reset password page
-        const frontendURL = process.env.FRONTEND_URL || 'http://localhost:5173';
+        const frontendURL = process.env.FRONTEND_URL || 'http://localhost:5173/arixelAI';
         const resetURL = `${frontendURL}/reset-password?id=${user.userId}&token=${token}`;
 
         // Send email using Resend HTTP API (Port 443/HTTPS is not blocked by Render)
