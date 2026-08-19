7c6273029fa40721ba6113462a7112943eeda252
Jotish <jotish@gmail.com>
2026-08-19 14:54:19 +0530

    fixed bugs in falClient.js

--- server/utils/falClient.js
+++ server/utils/falClient.js
@@ -1,8 +1,8 @@
 const { fal } = require("@fal-ai/client");
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
-const falAI = fal.config({
+fal.config({
     credentials: process.env.FAL_API_KEY,
 });
 
-module.exports = falAI
+module.exports = fal;
